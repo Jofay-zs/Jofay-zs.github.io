bfb9f0128e26242a181b227b7963eb5895f1c22c
Jofay-zs <jofayzs19@gmail.com>
2022-01-04 16:14:07 -0500

    Third attempt to fix the deployed project

--- src/components/indexComponents/home.js
+++ src/components/indexComponents/home.js
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <section className="flex w-full h-screen items-center justify-center" id="home">
       <StaticImage
-        className="absolute top-0 left-0 bg-cover z-10 h-screen w-full"
+        className="absolute top-0 left-0 bg-cover z-10 h-full w-full"
         alt="A nice photo taken in Gablenz, Germany. By Martin Damboldt (Pexels)"
         src="https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
       />
-      <div className="absolute top-0 left-0 z-20 bg-gradient-to-r from-main via-gray-900/75 h-screen w-full"></div>
+      <div className="absolute top-0 left-0 z-20 bg-gradient-to-r from-main via-gray-900/75 h-full w-full"></div>
       <div className="z-30 flex flex-col w-full h-full justify-center items-start sm:px-20">
         <div className="w-20 h-1 flex bg-gradient-to-r from-red-500 to-fuchsia-500"></div>
         <div>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,13 @@
 import React from "react";
 import General from "../layouts/general";
+import "../styles/global.css";
 
 const blog = () => {
-  return <General pageTitle={"Jofay's blog"}>
-    <main>
-      This is the blog
-    </main>
-  </General>;
+  return (
+    <General pageTitle={"Jofay's blog"}>
+      <main>This is the blog</main>
+    </General>
+  );
 };
 
 export default blog;
